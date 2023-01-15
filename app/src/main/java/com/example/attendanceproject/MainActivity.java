4dfd8659cfaa17c34f46af3ce5391acd785b6c93
Abraham Seyoum <abrahamseyoum06@gmail.com>
2023-01-15 22:12:52 +0300

    start and login page

--- app/src/main/java/com/example/attendanceproject/MainActivity.java
+++ app/src/main/java/com/example/attendanceproject/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.attendanceproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start =(Button)findViewById(R.id.buttonstart);
+        start.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Intent intent =new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
