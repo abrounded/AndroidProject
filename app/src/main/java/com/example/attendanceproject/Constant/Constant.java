870d14a7c808113c245100fdb89a7c655e7b2658
Mujammil-ios <mujammilpinjari68507@gmail.com>
2023-01-15 22:59:54 +0500

    Splash Screen and data binding added

---
+++ app/src/main/java/com/example/attendanceproject/Constant/Constant.java
@@ -0,0 +1,12 @@
+package com.example.attendanceproject.Constant;
+
+public class Constant {
+    /**
+     * delay in second
+     */
+    public static final int DELAY_SPLASH = 3000;
+    public static final int DELAY_FRAGMENT_PAUSE = 100;
+    public static final int DELAY_SCROLL_PAUSE = 300;
+    public static final int DELAY_API_REQUEST = 1000;
+    public static final int DELAY_MAP_MOVE = 2000;
+}
